743cf3397cd97f48b0528335c645e74479578400
LucasArmanelli <luc.armanelli@gmail.com>
2022-05-31 18:05:17 -0300

    Added Pages & Routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Developed by Lucas Armanelli</h1>
+    </div>
+  )
+}
+
+export default About
