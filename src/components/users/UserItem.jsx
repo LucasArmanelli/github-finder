de886f777f3890d79ac8f56d1453520e8fe950d0
LucasArmanelli <luc.armanelli@gmail.com>
2022-07-08 10:13:46 -0300

    Styles Adjustments

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 function UserItem({ user: { login, avatar_url } }) {
   return (
-    <div className='card shadow-md compact side bg-base-100'>
+    <div className='card shadow-md compact side bg-base-300 p-5 items-center text-center'>
       <div className="flex-row items-center space-x-4 card-body">
         <div>
           <div className="avatar">
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -98,7 +98,7 @@ function User() {
               </div>
             </div>
 
-            <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+            <div className='w-full rounded-lg shadow-md bg-base-300 stats'>
               {location && (
                 <div className='stat'>
                   <div className='stat-title text-md'>Location</div>
@@ -133,47 +133,33 @@ function User() {
           </div>
         </div>
 
-        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-300 stats'>
           <div className='grid grid-cols-1 md:grid-cols-3'>
-            <div className='stat'>
-              <div className='stat-figure text-secondary'>
-                <FaUsers className='text-3xl md:text-5xl' />
-              </div>
+
+            <div className='stat place-items-center'>
+              <FaUsers className='text-3xl md:text-5xl mr-4 mb-3'/>
               <div className='stat-title pr-5'>Followers</div>
               <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {followers}
               </div>
             </div>
 
-            <div className='stat'>
-              <div className='stat-figure text-secondary'>
-                <FaUserFriends className='text-3xl md:text-5xl' />
-              </div>
+            <div className='stat place-items-center'>
+              <FaUserFriends className='text-3xl md:text-5xl mr-4 mb-3'/>
               <div className='stat-title pr-5'>Following</div>
               <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {following}
               </div>
             </div>
 
-            <div className='stat'>
-              <div className='stat-figure text-secondary'>
-                <FaCodepen className='text-3xl md:text-5xl' />
-              </div>
+            <div className='stat place-items-center'>
+              <FaCodepen className='text-3xl md:text-5xl mr-4 mb-3'/>
               <div className='stat-title pr-5'>Public Repos</div>
               <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {public_repos}
               </div>
             </div>
-
-            <div className='stat'>
-              <div className='stat-figure text-secondary'>
-                <FaStore className='text-3xl md:text-5xl' />
-              </div>
-              <div className='stat-title pr-5'>Public Gists</div>
-              <div className='stat-value pr-5 text-3xl md:text-4xl'>
-                {public_gists}
-              </div>
-            </div>
+            
           </div>
         </div>
 
